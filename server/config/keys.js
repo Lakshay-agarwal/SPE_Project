052badcf0d628534de92efeadcbf1780fbb89a5a
Lakshay-agarwal <lakshayagarwal0@gmail.com>
2021-04-25 19:59:54 +0530

    Backend for Register and Login up

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: `mongo "mongodb+srv://cluster1.sd82w.mongodb.net/RoomDatabase" --username lakshay`
+    mongoURI: "mongodb+srv://lakshay:Gurgaon1@cluster1.sd82w.mongodb.net/RoomDatabase?retryWrites=true&w=majority" ,
+    secretOrKey: "secret"
   };
